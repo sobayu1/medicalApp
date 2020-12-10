cc2af1ce2c527e55676511a62b13778efb2f8c0b
github sobayu1 <yuzu5ayyf@gmail.com>
2020-12-10 15:28:12 +0900

    consultations has_many記述箇所移動

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -1,12 +1,11 @@
 class Consultation < ApplicationRecord
   belongs_to :user　,optional: true
   belongs_to :doctor ,optional: true
+  has_many :rooms, dependent: :destroy
   validates :user_id, presence: true
   validates :what_symptom, presence: true
   validates :when_symptom, presence: true
   validates :desease_treated, presence: true
   validates :desease_name, presence: true
   validates :specific_consultation, presence: true
-  has_many :rooms, dependent: :destroy
-  
 end
