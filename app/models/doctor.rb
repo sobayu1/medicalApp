c19617f0a930d7d17296e1b228b3a5e3f5e87cb9
github sobayu1 <yuzu5ayyf@gmail.com>
2020-12-10 19:13:05 +0900

    room_messagesテーブルへのreferences追加に伴いモデルへアソシエーション記述

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,6 +1,7 @@
 class Doctor < ApplicationRecord
   has_one :doctor_information, dependent: :destroy 
   has_many :consultations, dependent: :destroy
+  has_many :room_messages, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 has_one :user_information, dependent: :destroy 
 has_many :consultations, dependent: :destroy
+has_many :room_messages, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
