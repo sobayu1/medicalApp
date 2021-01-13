7f5827a03ba03eb36bfc54cfbee384fe97d27bc4
github sobayu1 <yuzu5ayyf@gmail.com>
2021-01-13 20:10:03 +0900

    fix:  strong_parameters修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        added_attrs = [ :name, :sex, :birth_date, :name, :email, :password, :password_confirmation, :image, tag_ids: []]
+        added_attrs = [ :name, :sex, :birth_date, :email, :password, :password_confirmation, :image, tag_ids: []]
         devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
         devise_parameter_sanitizer.permit :account_update, keys: added_attrs
         devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
