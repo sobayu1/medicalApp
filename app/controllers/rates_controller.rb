fb553e4d39902aec9d5d9f8857a1857087b3dcaa
github sobayu1 <yuzu5ayyf@gmail.com>
2021-01-19 14:29:47 +0900

    fix: doctor_idを受け渡す為、コード追加

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -2,5 +2,23 @@ class RatesController < ApplicationController
 
     def new
         @rate = Rate.new
+        @doctor = params[:doctor_id]
+    end
+
+    def create
+        @rate = Rate.new(rate_params)
+        if @rate.save!
+            flash[:success] = "アンケートに回答しました"
+            redirect_to user_path(current_user)
+        else
+            flash[:danger] = "アンケートの回答に失敗しました"
+            redirect_to new_rate_path
+        end
+
+    end
+
+    private
+    def rate_params
+        params.require(:rate).permit(:doctor_rate, :doctor_id).merge(user_id: current_user.id )
     end
 end
