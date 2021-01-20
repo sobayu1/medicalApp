1c8b26dbf0f52b22db320cd1f437074d048c1958
github sobayu1 <yuzu5ayyf@gmail.com>
2021-01-20 19:40:15 +0900

    fix: rate保存のため、controllerおよびview変更

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,8 +1,9 @@
 class RatesController < ApplicationController
 
     def new
-        @rate = Rate.new
+        @consultation = Consultation.find(params[:consultation_id])
         @doctor = params[:doctor_id]
+        @rate = Rate.new
     end
 
     def create
@@ -12,13 +13,12 @@ class RatesController < ApplicationController
             redirect_to user_path(current_user)
         else
             flash[:danger] = "アンケートの回答に失敗しました"
-            redirect_to new_rate_path
+            redirect_to new_consultation_rate_path
         end
-
     end
 
     private
     def rate_params
-        params.require(:rate).permit(:doctor_rate, :doctor_id).merge(user_id: current_user.id )
+        params.require(:rate).permit(:doctor_rate, :doctor_id, :consultation_id).merge(user_id: current_user.id)
     end
 end
